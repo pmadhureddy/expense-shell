9edeac88acdd9d089981388eb4ebdbef24a697a2
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-31 23:07:08 +0530

    Added shell script for the front end code...

---
+++ frontend.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+
+LOGS_FOLDER="/var/logs/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+echo "Script started excecuting at: $(date)" | tee -a $LOG_FILE
+
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then echo -e "$2 is ..... $R Failed $N" | tee -a $LOG_FILE
+         exit 1
+    else
+        echo -e "$2 is .... $G succcess $N" | tee -a $LOG_FILE
+    fi         
+}
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run this script with root privileges" | tee -a $LOG_FILE
+   exit 1
+fi  
+
+
+dnf install nginx -y 
+VALIDATE $? "Installing Nginx..."
+
+
+systemctl enable nginx
+VALIDATE $? "Enabled Nginx..."
+
+
+systemctl start nginx
+VALIDATE $? "Started Nginx..."
+
+
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip
+VALIDATE $? "Downloading frontend code..."
+
+
+cd /usr/share/nginx/html
+
+
+unzip /tmp/frontend.zip
+VALIDATE $? "Extracting the frontend code..."
+
+systemctl restart nginx
+VALIDATE $? "Restarting nginx..."
