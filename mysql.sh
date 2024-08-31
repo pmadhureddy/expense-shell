e3da1ac89be92a140563728252b442e3285abd3f
pmadhureddy <pmadhureddy1510@gmail.com>
2024-08-31 16:09:31 +0530

    written shell script foor the mysql setup

---
+++ mysql.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+
+LOGS_FOLDER="/var/logs/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+
+echo "Script started excecuting at: $(date)" | tee -a $LOG_FILE
+
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]
+    then echo -e "$2 is ..... $R Failed $N" | tee -a $LOG_FILE
+         exit 1
+    else
+        echo -e "$2 is .... $G succcess $N" | tee -a $LOG_FILE
+    fi         
+}
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run this script with root privileges" | tee -a $LOG_FILE
+   exit 1
+fi  
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MySQL-server"     
+
+systemctl enable mysqld
+VALIDATE $? "Enabled MySQL server"     
+
+systemctl start mysqld
+VALIDATE $? "Started MySQL server"     
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting up root password"     
+
+
